97445569ea2624067eaaa8f87451894653e9814d
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-15 23:28:22 +0200

    fix: adds literal type in basic-3.ts

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -2,3 +2,9 @@ let stringOrNumber: string | number = 2;
 stringOrNumber = "somestring";
 
 console.log(stringOrNumber);
+
+let enableOrDisabled: "enabled" | "disabled" = "disabled";
+
+enableOrDisabled = "enabled";
+
+console.log(enableOrDisabled);
